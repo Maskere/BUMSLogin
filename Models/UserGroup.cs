30a6b29fea51332fcec49d3b71f06cc2f351edbf
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2024-11-27 12:37:11 +0100

    Udkommenteret

--- Models/UserGroup.cs
+++ Models/UserGroup.cs
@@ -15,16 +15,16 @@ namespace BUMS.Models{
         [Column("Access_Id")]
         public int AccessID{get;set;}
 
-        [ForeignKey(nameof(UserID))]
-        [InverseProperty(nameof(User.UserUserGroups))]
-        public virtual UserGroup UserUserGroupNavigation {get;set;}
+        //[ForeignKey(nameof(UserID))]
+        //[InverseProperty(nameof(User.UserUserGroups))]
+        //public virtual UserGroup UserUserGroupNavigation {get;set;}
 
-        [ForeignKey(nameof(AccessID))]
-        [InverseProperty(nameof(Access.AccessUserGroups))]
-        public virtual Access AccessUserGroupNavigation {get;set;}
+        //[ForeignKey(nameof(AccessID))]
+        //[InverseProperty(nameof(Access.AccessUserGroups))]
+        //public virtual Access AccessUserGroupNavigation {get;set;}
 
-        [ForeignKey(nameof(GroupID))]
-        [InverseProperty(nameof(Group.GroupUserGroups))]
-        public virtual Group GroupUserGroupNavigation {get;set;}
+        //[ForeignKey(nameof(GroupID))]
+        //[InverseProperty(nameof(Group.GroupUserGroups))]
+        //public virtual Group GroupUserGroupNavigation {get;set;}
     }
 }
