12d4bbcf4f8c8993a06f11bd9fc7b30a421aeff9
Gubbiii <lucasrm@live.dk>
2024-11-27 10:38:34 +0100

    Access model

--- Models/Access.cs
+++ Models/Access.cs
@@ -1,6 +1,22 @@
-﻿namespace BUMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BUMS.Models
 {
     public class Access
     {
+
+        public Access() { }
+
+        [Required]
+        [Key]
+        [Column("Access_id")]
+        public string AccessID { get; set; }
+
+        [Column("Access_Name")]
+        public string AccessName { get; set; }
+
+        [Column("System_Name")]
+        public string SystemName { get; set; }
     }
 }
